fa41004815f9e2f156e2a5a80991dd579c1f4464
Roshan <rkverma9730@gmail.com>
2026-06-27 00:02:07 +0530

    Fix : Migrated form numeric Status Code to http-status-codes library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,37 @@
+const { StatusCodes } = require("http-status-codes");
+
 function pingProblemController(req, res) {
   return res.json({ message: "ping controller is up !!" });
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({ message: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({ message: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({ message: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({ message: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 function updateProblem(req, res) {
-  return res.status(501).json({ message: "Not Implemented" });
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: "Not Implemented" });
 }
 
 module.exports = {
@@ -30,3 +42,8 @@ module.exports = {
   updateProblem,
   pingProblemController,
 };
+
+/**
+ * res.status -> return the same response object with the status property set -> first it set the status property to specified code and return the same code
+ * res.status().json -> return the same response object which has the status set but this time json to be returned is also set
+ */
